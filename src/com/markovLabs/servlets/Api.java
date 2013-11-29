856d807e42e576af26307c81123eba953754f147
Victor Regalado <vrrs0988@gmail.com>
2013-11-29 00:53:56 -0500

    fixed the json parser issue. It works properly now

--- src/com/markovLabs/servlets/Api.java
+++ src/com/markovLabs/servlets/Api.java
@@ -10,7 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 @WebServlet("/api")
 public class Api extends HttpServlet {
@@ -30,38 +31,41 @@ public class Api extends HttpServlet {
 	// operations: create, delete bid and proccess bid
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, java.io.IOException {
-		
+
 		BufferedReader reader = req.getReader();
 		StringBuilder buf = new StringBuilder();
 		String line = null;
 		while ((line = reader.readLine()) != null) {
 			buf.append(line);
 		}
-		JSONObject json = new JSONObject(buf.toString());
-		Integer id = (Integer) json.get(USERID_FIELD);
-		Integer operation = (Integer) json.get(OPERATION_FIELD);
-		
-		String mesg="SUCCESS"; // message returned back to the client
-		if (id == null || operation == null) {
-			mesg = "FAIL: JSON string not valid";
+		JSONObject json = (JSONObject) JSONValue.parse(buf.toString());
+
+		String mesg = "SUCCESS"; // message returned back to the client
+		if (json == null) {
+			mesg = "FAIL: No JSON string was received.";
 		} else {
-			switch (operation) {
-				case CREATE_OP:createBid(id);break;
-				case DELETE_OP:deleteBid(id);break;
-				case PROCESS_BID:processBid(json);break;
-				default: mesg="FAIL: Invalid operation code.";
+			Object id = json.get(USERID_FIELD);
+			Object operation = (Integer) json.get(OPERATION_FIELD);
+
+			if (id == null || operation == null) {
+				mesg = "FAIL: JSON string not valid";
+			} else {
+				switch ((Integer) operation) {
+					case CREATE_OP:createBid((Integer) id);break;
+					case DELETE_OP:deleteBid((Integer) id);break;
+					case PROCESS_BID:processBid(json);break;
+					default:mesg = "FAIL: Invalid operation code.";
+				}
 			}
 		}
-		
-		PrintWriter printer=resp.getWriter();
-		try{
+		PrintWriter printer = resp.getWriter();
+		try {
 			printer.print(mesg);
 			printer.close();
+		} catch (Exception e) {
+
 		}
-		catch(Exception e){
-			
-		}
-		
+
 	}
 
 	private void processBid(JSONObject json) {

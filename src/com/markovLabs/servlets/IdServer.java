6b7aa988900416da975857f4b8ecffdbc7b31d40
Victor Regalado <vrrs0988@gmail.com>
2013-12-01 21:02:24 -0500

    updated the id servlet so it includes search functionality

--- src/com/markovLabs/servlets/IdServer.java
+++ src/com/markovLabs/servlets/IdServer.java
@@ -1,6 +1,7 @@
 package com.markovLabs.servlets;
 
 import java.io.BufferedReader;
+import java.io.PrintWriter;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,8 +18,12 @@ import org.json.simple.JSONValue;
 @WebServlet("/id")
 public class IdServer  extends HttpServlet {
 	
+	private static final long serialVersionUID = 1L;
+	private static final String OPERATION_FIELD = "op";
 	private int counter=0;
 	private final Set<Integer> ids=new HashSet<Integer>();
+	
+	private static String USERID_FIELD="id";
 
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
@@ -33,12 +38,37 @@ public class IdServer  extends HttpServlet {
 			buf.append(line);
 		}
 		JSONObject json = (JSONObject) JSONValue.parse(buf.toString());
-		
-		
+		StringBuilder mesg=new StringBuilder();
+		if (json == null) {
+			mesg.append("FAIL: No valid JSON string was received.");
+		} else {
+			Integer id =(Integer) json.get(USERID_FIELD);
+			Integer operation = (Integer)json.get(OPERATION_FIELD);
+			switch(operation){
+				case 0:mesg.append(createID());break;
+				case 1:mesg.append(search(id));break;
+			}
+		}
+		PrintWriter printer = resp.getWriter();
+		try {
+			printer.print(mesg.toString());
+			printer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	private String search(Integer id){
+		return Boolean.toString(ids.contains(id));
+	}
+	
+	private Integer createID(){
 		synchronized(this){
 			counter++;
 			ids.add(new Integer(counter));
 		}
+		return counter;
 	}
 	
 }
